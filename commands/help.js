af4d860eca3f22049777e95fd2ca96104b28b07b
Wruckles <77592032+Wruckles@users.noreply.github.com>
2021-01-18 13:24:16 -0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
         })
 
         let embed = new MessageEmbed()
-        .setAuthor("Commands of "+client.user.username, "https://raw.githubusercontent.com/Wruckles/Wruckusbot/main/assets/mangekyo%20sharingan.gif")
+        .setAuthor("Commands of "+client.user.username, "https://raw.githubusercontent.com/Wruckles/Wruckusbot/main/assets/Music.gif?token=ASP7LYGAOU2UYDZRRQZOUTLAAX6H2")
         .setColor("PURPLE")
         .setDescription(allcmds)
         .setFooter(`To get info of each command you can do ${client.config.prefix}help [command] | Hander by ᲼᲼᲼᲼᲼᲼Wruck
