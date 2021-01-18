3c3f5c41d970c755dd4712479c99031443cd87c8
Wruckles <77592032+Wruckles@users.noreply.github.com>
2021-01-18 14:09:38 -0800

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -16,7 +16,7 @@ module.exports = {
       serverQueue.connection.dispatcher.resume();
       let xd = new MessageEmbed()
       .setDescription("▶ Resumed the music for you!")
-      .setColor("YELLOW")
+      .setColor("RED")
       .setAuthor("Music has been Resumed!", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
       return message.channel.send(xd);
     }
