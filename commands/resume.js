85fe6b6d067ea239a42ad5b08f60b7ea31939a31
Wruckles <77592032+Wruckles@users.noreply.github.com>
2021-01-18 03:42:52 -0800

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -17,7 +17,7 @@ module.exports = {
       let xd = new MessageEmbed()
       .setDescription("▶ Resumed the music for you!")
       .setColor("PURPLE")
-      .setAuthor("Music has been Resumed!", "https://raw.githubusercontent.com/Wruckles/Wruckusbot/master/assets/mangekyo sharingan.gif")
+      .setAuthor("Music has been Resumed!", "https://raw.githubusercontent.com/Wruckles/Wruckusbot/assets/mangekyo sharingan.gif")
       return message.channel.send(xd);
     }
     return sendError("There is nothing playing in this server.", message.channel);
