dfa255b56cd810f1ecd56257dcc3290e7799e981
Wruckles <77592032+Wruckles@users.noreply.github.com>
2021-01-18 13:42:54 -0800

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -16,13 +16,13 @@ module.exports = {
     if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
     if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
      if(isNaN(args[0])) return message.channel.send(':notes: Numbers only!').catch(err => console.log(err));
-    if(parseInt(args[0]) > 100 ||(args[0]) < 0) return sendError('You can\'t set the volume more than 100. or lower than 0',message.channel).catch(err => console.log(err));
+    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('You can\'t set the volume more than 150. or lower than 0',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
-    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100;
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let xd = new MessageEmbed()
     .setDescription(`I set the volume to: **${args[0]/1}/100**`)
     .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
-    .setColor("PURPLE")
+    .setColor("RED")
     return message.channel.send(xd);
   },
 };
