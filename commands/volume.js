8ff24e2555f7e768479c6a4e95e0c30f41efce90
Wruckles <77592032+Wruckles@users.noreply.github.com>
2021-01-24 22:03:51 -0800

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -21,7 +21,7 @@ module.exports = {
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let xd = new MessageEmbed()
     .setDescription(`I set the volume to: **${args[0]/1}/100**`)
-    .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+    .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/Wruckles/Wruckusbot/main/assets/sh.gif?token=ASP7LYAS4DNDXHVPVYNG5HTABZO4U")
     .setColor("RED")
     return message.channel.send(xd);
   },
