ab966fd49617a4e3bb5ce0868bc791246305fc28
Wruckles <77592032+Wruckles@users.noreply.github.com>
2021-01-25 20:23:12 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("You Taking A Shit", {
+    type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
